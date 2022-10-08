119dcdbfa116e70bc21a60da93ec460e4eedd593
URK21CS1072 <100009296+URK21CS1072@users.noreply.github.com>
2022-10-08 22:38:39 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,26 +1,24 @@
 from typing import List
-
-# divide function
-def partition(arr,low,high):
-   i = ( low-1 )
-   pivot = arr[high] # pivot element
-   for j in range(low , high):
-      # If current element is smaller
-      if arr[j] <= pivot:
-         # increment
-         i = i+1
-         arr[i],arr[j] = arr[j],arr[i]
-   arr[i+1],arr[high] = arr[high],arr[i+1]
-   return ( i+1 )
-# sort
-def quick_sort(arr,low,high):
-   if low < high:
-      # index
-      pi = partition(arr,low,high)
-      # sort the partitions
-      quick_sort(arr, low, pi-1)
-      quick_sort(arr, pi+1, high)
-   return arr
+# to partition and sort the array
+# it takes 3 values the data low end and highr end
+def sorting(data, low ,high):
+    i=low-1
+    pivot = data[high]
+    for j in range(low,high):
+        if data[j] <= pivot:
+            i+=1
+            data[i], data[j] =  data[j], data[i]
+    data[i+1], data[high] = data[high],data[i+1]
+    return i+1
+# the basic code of quick sort to divide it from the pivot element
+def quick_sort(data, low, high) -> List[int]:
+    if low<high:
+        pi = sorting(data,low,high)
+        quick_sort(data,low,pi-1)
+        quick_sort(data,pi+1,high)
+        return data
+        
+    
 
 input_data = input()
 data = []
